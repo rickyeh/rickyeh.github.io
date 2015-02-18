f572d19b552c67df63d39190ddcb64c8559220cb
RickYeh <RickYeh@users.noreply.github.com>
2015-02-17 23:08:04 -0800

    Added console.log tests and barebones functionaity

--- roulette/roulette.js
+++ roulette/roulette.js
@@ -14,14 +14,14 @@ var player = {
     // Params:
     //      amount - amount of chips that is bet
     //      number - the number that is bet on
-    bet : function bet(amount, number){
+    bet : function (amount, number){
         player.bankroll = player.bankroll - amount;
         player.betAmt = amount;
         player.betNumber = number;
 
-        console.log("I am betting " + amount + " chips on " + number + ".");
+        console.log('I am betting ' + amount + ' chips on ' + number + '.');
     }
-}
+};
 
 // Object to represent the table in the roulette game
 // Properties :
@@ -30,36 +30,46 @@ var player = {
 // Methods:
 //     spin() - The function called when wheel is spun
 //     payWinner() - Finds any winning bets and pays out appropriate amount
-var rTable = {
+var table = {
     currentNum : 0,
     payoutAmt: 0,
     possibleNums : [],
 
-    spin : function spin(){
-        console.log("Table is spun.");
-        rTable.currentNum = rTable.possibleNums[Math.floor(Math.random()*rTable.possibleNums.length)];
-        console.log("Current Number: " + rTable.currentNum);
+    spin : function (){
+        console.log('Table is spun.');
+        table.currentNum = table.possibleNums[Math.floor(Math.random()*table.possibleNums.length)];
+        console.log('Current Number: ' + table.currentNum);
     },
 
-    payWinner : function payWinner(){
-        
+    payWinner : function (betAmt){
+        if (player.betNumber.toString() === this.currentNum) {
+            console.log('Congratulations, you won!');
+            this.payoutAmt = betAmt * 35;
+            player.bankroll = player.bankroll + this.payoutAmt;
+        } else {
+            console.log('Sorry, try again!');
+        }
+        console.log(player.bankroll);
+        console.log('Current Bankroll is : ' + player.bankroll);
     }
-}
+};
 
 // MAIN PROGRAM STARTS HERE
 
 // Form the array of possible winning numbers
-rTable.possibleNums = ['00', '0', '1', '2'];
+table.possibleNums = ['00', '0', '1', '2'];
 
 // Start game with 100 chips
 player.bankroll = 100;
+player.betAmt = 10;
 
 // Player makes bets, subtract amount of money made
 player.bet(player.betAmt, player.betNumber);
 
 // Player hits spin button, table spins
 // Randomize the current number to be one of the possible possibleNums.
-rTable.spin();
+table.spin();
 
 // If player's bet number matches the currentNum, pay out the player.
     // if not, clear the board and start again
+table.payWinner(player.betAmt);
