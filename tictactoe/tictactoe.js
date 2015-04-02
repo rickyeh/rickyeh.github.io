112b6786b4c20cee3492dde0b6bbb8e464665fad
RickYeh <RickYeh@users.noreply.github.com>
2015-04-01 21:47:31 -0700

    Changed array to 2 Dimensional, and updated XO placement logic

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -10,7 +10,9 @@
 var currentPlayer = 0;  // 0 for O, 1 for X
 var totalTurns = 0;     // increment after every move
 
-var gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var gameBoard = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]];
 
 // var player = {
 //     sign: 0,
@@ -27,26 +29,29 @@ function player(sign) {
                 
 var boardUI = {
 
-    oString: "<svg class='xo' height='100' width='100'>" +
-                "<circle cx='50' cy='50' r='45' stroke='#E43A20' stroke-width='10' fill-opacity='0' />" + 
-             "</svg>",
+    oString: '<svg class="xo" height="100" width="100">' +
+                '<circle cx="50" cy="50" r="43" stroke="#E43A20" stroke-width="12" fill-opacity="0" />' + 
+             '</svg>',
 
-    xString: "<svg class='xo' height='100' width='100'>" +
-                "<line x1='10' y1='10' x2='90' y2='90' style='stroke:#00AAC4;stroke-width:10' />" +
-                "<line x1='90' y1='10' x2='10' y2='90' style='stroke:#00AAC4;stroke-width:10' />" +
-             "</svg>",
+    xString: '<svg class="xo" height="100" width="100">' +
+                '<line x1="10" y1="10" x2="90" y2="90" style="stroke:#00AAC4;stroke-width:12" />' +
+                '<line x1="90" y1="10" x2="10" y2="90" style="stroke:#00AAC4;stroke-width:12" />' +
+             '</svg>',
 
     createClickHandlers: function() {
 
         // Create anonymous function to pass in the i use closure
         function createAnonFunction(i) {
             var actionOnClick = function() {
-                console.log('Clicked box');
                 if (currentPlayer === 0) {
                     $('#box' + i).hide().html(boardUI.oString).fadeIn('fast');
+                    gameBoard[Math.floor(i/3)][i%3] = 1;
+                    boardUI.checkVictory(1);
                     currentPlayer++;
                 } else {
                     $('#box' + i).hide().html(boardUI.xString).fadeIn('fast');
+                    gameBoard[Math.floor(i/3)][i%3] = 2;
+                    boardUI.checkVictory(2);
                     currentPlayer--;
                 }
             };
@@ -60,7 +65,8 @@ var boardUI = {
         }
     },
 
-    checkVictory: function() {
+    checkVictory: function(n) {
+
     }
 };
 
