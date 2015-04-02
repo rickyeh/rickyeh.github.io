3ddb83593783d39a350e6ab1279b262be274eb4b
RickYeh <RickYeh@users.noreply.github.com>
2015-04-02 14:29:10 -0700

    Updated refresh button

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -48,13 +48,10 @@ var playerO = new Player('O');
 var boardUI = {
 
     oString: '<svg class="xo" height="110" width="110">' +
-                '<circle cx="56" cy="57" r="43" stroke="#666666" stroke-width="12" fill-opacity="0" />' + 
                 '<circle cx="55" cy="55" r="43" stroke="#E43A20" stroke-width="12" fill-opacity="0" />' + 
              '</svg>',
 
     xString: '<svg class="xo" height="100" width="100">' +
-                '<line x1="11" y1="12" x2="91" y2="92" style="stroke:#666666;stroke-width:12" />' +
-                '<line x1="91" y1="12" x2="11" y2="92" style="stroke:#666666;stroke-width:12" />' +
                 '<line x1="10" y1="10" x2="90" y2="90" style="stroke:#00AAC4;stroke-width:12" />' +
                 '<line x1="90" y1="10" x2="10" y2="90" style="stroke:#00AAC4;stroke-width:12" />' +
              '</svg>',
@@ -78,8 +75,15 @@ var boardUI = {
         }
 
         $('#resetButton').click(function() {
-            location.reload();
+            for (var i = 0; i < 9; i++) {
+                $('#box' + i).fadeOut('slow');
+            }
+            window.setTimeout(boardUI.reloadPage, 500);
         });
+    },
+
+    reloadPage: function() {
+        location.reload();
     },
 
     checkVictory: function() {
